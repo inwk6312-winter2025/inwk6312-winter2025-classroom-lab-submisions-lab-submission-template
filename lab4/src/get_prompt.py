22f038ea7274966ecc4f9fbc1eba3fa8e9be9dea
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2025-02-13 10:20:09 -0400

    Task 1 Modified

--- lab4/src/get_prompt.py
+++ lab4/src/get_prompt.py
@@ -5,8 +5,15 @@ device = {
     "ip": "192.168.1.101", # R1 Mgmt Interface
     "username": "student",
     "password": "Meilab123",
+    "secret": "cisco",
     "port": "22",
     }
 
 net_connect = Netmiko(**device)
-print(net_connect.find_prompt())
+print(f"Default prompt: {net_connect.find_prompt()}")
+
+net_connect.send_command_timing("disable")
+print(f"Disable command: {net_connect.find_prompt()}")
+
+net_connect.enable()
+print(f"Enable command: {net_connect.find_prompt()}")
